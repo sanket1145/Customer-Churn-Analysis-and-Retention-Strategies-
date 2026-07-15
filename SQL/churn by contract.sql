114bde01f7cfce4ad08ce22c76d51bbb6a8258b3
Sanket Vinayak Patil <sanketpatil1142005@gmail.com>
2026-07-15 16:22:12 +0530

    Add files via upload

---
+++ SQL/churn by contract.sql
@@ -0,0 +1,11 @@
+SELECT 
+    Contract,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate_percent
+FROM churn_data
+GROUP BY Contract
+ORDER BY churn_rate_percent DESC;
